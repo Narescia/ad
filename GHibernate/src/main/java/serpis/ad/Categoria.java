e266fe7cbd4ef8fc6c9e5aa171947b181f491be5
Narescia <narescia@gmail.com>
2017-12-20 13:12:12 +0100

    Mejorado GHibernate

--- GHibernate/src/main/java/serpis/ad/Categoria.java
+++ GHibernate/src/main/java/serpis/ad/Categoria.java
@@ -1,12 +1,15 @@
 package serpis.ad;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 
 public class Categoria {
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	private String nombre;
 	
@@ -18,6 +21,14 @@ public class Categoria {
 		this.nombre = nombre;
 	}
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("[%s] %s", id, nombre);
--- GHibernate/src/main/java/serpis/ad/PruebaHibernate.java
+++ GHibernate/src/main/java/serpis/ad/PruebaHibernate.java
@@ -2,6 +2,8 @@ package serpis.ad;
 
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,11 +12,14 @@ public class PruebaHibernate {
 
 	private static EntityManagerFactory entityManagerFactory;
 	public static void main(String[] args) {
+		Logger.getLogger("org.hibernate").setLevel(Level.OFF);
 		entityManagerFactory = 
 			Persistence.createEntityManagerFactory("serpis.ad.ghibernate");
 		
 		showAll();
 		modify(1L);
+		remove(2L);
+		newCategoria();
 		showAll();
 	
 		entityManagerFactory.close();
@@ -24,17 +29,44 @@ public class PruebaHibernate {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		List <Categoria> categorias = 
-			entityManager.createQuery("from Categoria", Categoria.class).getResultList();
+			entityManager.createQuery("from Categoria order by id", Categoria.class).getResultList();
 		for (Categoria categoria : categorias)
 			System.out.println(categoria);
 		entityManager.getTransaction().commit();
 	}
 	
-	private static void modify (long id) {
+	private static void newCategoria () {
+		System.out.println("creando una nueva categoría");
+		Categoria categoria = new Categoria();
+		categoria.setNombre("nuevo " + new Date());
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		Categoria categoria = entityManager.find(Categoria.class, id);
+		System.out.println("Creando: " + categoria);
+		entityManager.persist(categoria);
+		System.out.println("Creada: " + categoria);
+		entityManager.getTransaction().commit();
+	}
+	
+	private static void modify (long id) {
+		System.out.println("modificando categoría " + id);
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		//Categoria categoria = entityManager.find(Categoria.class, id);
+		Categoria categoria = new Categoria ();
+		categoria.setId(id);
 		categoria.setNombre("modificado " + new Date());
+		entityManager.persist(categoria);
+		entityManager.getTransaction().commit();
+	}
+	
+	private static void remove (long id) {
+		System.out.println("eliminando categoría " + id);
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		//Categoria categoria = entityManager.find(Categoria.class, id);
+		Categoria categoria = entityManager.getReference(Categoria.class, id);
+		// categoria.setId(id);
+		entityManager.remove(categoria);
 		entityManager.getTransaction().commit();
 	}
 
